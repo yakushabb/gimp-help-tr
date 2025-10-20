ec2ee433ebf9b9d261f40ac18b830b3bea5cef06
Sabri Ünal <yakushabb@gmail.com>
2025-10-20 20:45:44 +0300

    silebiliriz artık

---
+++ po/update.sh
@@ -0,0 +1,15 @@
+#!bash
+
+msgmerge --previous --update appendix.po ../pot/appendix.pot
+msgmerge --previous --update appendix.po ../pot/appendix.pot
+msgmerge --previous --update concepts.po ../pot/concepts.pot
+msgmerge --previous --update dialogs.po ../pot/dialogs.pot
+msgmerge --previous --update filters.po ../pot/filters.pot
+msgmerge --previous --update gimp.po ../pot/gimp.pot
+msgmerge --previous --update glossary.po ../pot/glossary.pot
+msgmerge --previous --update introduction.po ../pot/introduction.pot
+msgmerge --previous --update menus.po ../pot/menus.pot
+msgmerge --previous --update preface.po ../pot/preface.pot
+msgmerge --previous --update toolbox.po ../pot/toolbox.pot
+msgmerge --previous --update tutorial.po ../pot/tutorial.pot
+msgmerge --previous --update using.po ../pot/using.pot
